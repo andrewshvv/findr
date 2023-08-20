a1912e6c672d5388d07ce6cb15d21e61992e9df7
Andrey <andrew.shvv@gmail.com>
2023-08-20 12:38:12 +0300

    db: increase timeout

--- src/common/db.py
+++ src/common/db.py
@@ -5,9 +5,9 @@ import aiomisc
 from aiosqlite import Connection, Cursor
 
 
-@aiomisc.asyncbackoff(attempt_timeout=10,
-                      deadline=10,
-                      pause=0.1,
+@aiomisc.asyncbackoff(attempt_timeout=30,
+                      deadline=30,
+                      pause=0.5,
                       max_tries=10,
                       exceptions=(sqlite3.OperationalError,))
 async def safe_db_execute(db: Union[Connection, Cursor], *args, **kwargs) -> Cursor:
